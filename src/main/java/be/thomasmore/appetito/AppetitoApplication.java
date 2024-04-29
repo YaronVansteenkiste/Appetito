af4cf4daccfd768d7ebdcd2cd4e5fc2e03a804d0
ahmed <ahmed.kerkri@outlook.com>
2024-04-29 11:22:13 +0200

    test

---
+++ src/main/java/be/thomasmore/appetito/AppetitoApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.appetito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppetitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppetitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/appetito/AppetitoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.appetito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppetitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
