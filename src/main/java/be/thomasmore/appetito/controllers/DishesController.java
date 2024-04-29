0bb3af311e8f8f185c1c6d9549bb1ab267abe830
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-04-29 14:19:48 +0200

    Dishes controller aangemaakt

--- src/main/java/be/thomasmore/appetito/controllers/DishesController.java
+++ src/main/java/be/thomasmore/appetito/controllers/DishesController.java
@@ -1,8 +1,14 @@
 package be.thomasmore.appetito.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DishesController {
 
+    @GetMapping("/dishes")
+    public String dishes(Model model){
+        return "dishes";
+    }
 }
