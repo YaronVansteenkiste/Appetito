212081cede269004611762481c767439f7eb5313
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2024-04-29 14:00:51 +0200

    29/04: initiate project

---
+++ src/main/java/be/thomasmore/appetito/controllers/DishesController.java
@@ -0,0 +1,8 @@
+package be.thomasmore.appetito.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DishesController {
+
+}
--- src/main/java/be/thomasmore/appetito/controllers/HomeController.java
+++ src/main/java/be/thomasmore/appetito/controllers/HomeController.java
@@ -1,4 +1,16 @@
 package be.thomasmore.appetito.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String Home(Model model) {
+        return "homepage";
+    }
+
+
 }
