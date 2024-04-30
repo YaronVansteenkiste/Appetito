52278df525a767f205acde12720be765193802a1
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-04-30 10:16:37 +0200

    30/04 - About page

---
+++ src/main/java/be/thomasmore/appetito/controllers/AboutController.java
@@ -0,0 +1,13 @@
+package be.thomasmore.appetito.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+    @GetMapping("/aboutus")
+    public String About(Model model) {
+        return "about";
+    }
+}
