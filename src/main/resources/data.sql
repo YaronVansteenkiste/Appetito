0a7b03a320b79c6460c271196fe59e019b91e704
ahmedk1991 <ahmed.kerkri@outlook.com>
2024-04-30 12:55:44 +0200

    database verbeterd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,13 +5,13 @@ VALUES (
            20,
            'avondmaal',
            'pasta_alfredo.jpg',
-           '1. Begin met het koken van de fettuccine volgens de aanwijzingen op de verpakking. Vergeet niet een kopje van het kookvocht te bewaren voordat je de pasta afgiet.\n'
-               '2. Terwijl de pasta kookt, kruid je de kipstukjes naar smaak en bak je ze in een aparte pan tot ze volledig gaar en goudbruin zijn. Zet de kip even opzij.\n'
-               '3. Smelt de roomboter in een diepe pan op middelhoog vuur. Voeg het fijngehakte teentje knoflook toe en laat het een minuutje zachtjes bakken, zodat de smaken goed vrijkomen.\n'
-               '4. Voeg de gekookte fettuccine toe aan de pan met gesmolten boter en knoflook. Giet er een goed scheutje van het bewaarde kookvocht bij om de saus smeuïg te maken.\n'
-               '5. Strooi de geraspte Parmezaanse kaas over de pasta en roer alles goed door elkaar totdat de kaas volledig is gesmolten en een romige saus vormt.\n'
-               '6. Voeg nu de gebakken kipstukjes toe aan de pan en meng alles goed door elkaar. Breng op smaak met zwarte peper.\n'
-               '7. Serveer de pasta alfredo op borden en garneer met de gehakte peterselie voor een frisse touch.\n'
+           '1. Begin met het koken van de fettuccine volgens de aanwijzingen op de verpakking. Vergeet niet een kopje van het kookvocht te bewaren voordat je de pasta afgiet.'
+               '2. Terwijl de pasta kookt, kruid je de kipstukjes naar smaak en bak je ze in een aparte pan tot ze volledig gaar en goudbruin zijn. Zet de kip even opzij.'
+               '3. Smelt de roomboter in een diepe pan op middelhoog vuur. Voeg het fijngehakte teentje knoflook toe en laat het een minuutje zachtjes bakken, zodat de smaken goed vrijkomen.'
+               '4. Voeg de gekookte fettuccine toe aan de pan met gesmolten boter en knoflook. Giet er een goed scheutje van het bewaarde kookvocht bij om de saus smeuïg te maken.'
+               '5. Strooi de geraspte Parmezaanse kaas over de pasta en roer alles goed door elkaar totdat de kaas volledig is gesmolten en een romige saus vormt.'
+               '6. Voeg nu de gebakken kipstukjes toe aan de pan en meng alles goed door elkaar. Breng op smaak met zwarte peper.'
+               '7. Serveer de pasta alfredo op borden en garneer met de gehakte peterselie voor een frisse touch.'
                '8. Sandra’s tip: Voor een extra portie groen kun je deze heerlijke pasta alfredo serveren met gestoomde broccoli of erwtjes. Zo krijg je meteen je dagelijkse portie groenten binnen!'
        );
 
@@ -32,8 +32,8 @@ VALUES (
            30,
            'avondeten',
            'pasta_zalm.jpg',
-           '1. Kook de pasta volgens verpakking beetgaar. Snipper ondertussen de ui en de teentjes knoflook fijn. Bak deze glazig in een grote hapjes of wokpan in een eetlepel olie.\n'
-               '2. Voeg de zalm toe en bak rondom aan, steek met een spatel in stukken. Voeg dan in etappes de spinazie toe totdat deze wat geslonken is. Schenk de kookroom erbij en breng op maak met flink wat peper en zout.\n'
+           '1. Kook de pasta volgens verpakking beetgaar. Snipper ondertussen de ui en de teentjes knoflook fijn. Bak deze glazig in een grote hapjes of wokpan in een eetlepel olie.'
+               '2. Voeg de zalm toe en bak rondom aan, steek met een spatel in stukken. Voeg dan in etappes de spinazie toe totdat deze wat geslonken is. Schenk de kookroom erbij en breng op maak met flink wat peper en zout.'
                '3. Halveer de tomaatjes en meng deze met de zalm en spinazie. Verwarm een paar minuten door en voeg dan de tagliatelle toe. Mix alles door elkaar en schep op borden. Bestrooi met geroosterde pijnboompitten.'
        );
 
@@ -56,11 +56,11 @@ VALUES (
            'vegetarisch',
            20,
            'avondmaal','spinaziesoep.jpg',
-           '1. Snijd de ui fijn en bak in een pan met boter glazig.\n'
-               '2. Snijd de aardappel in blokjes. Voeg de aardappel en ontdooide spinazie toe aan de ui.\n'
-               '3. Giet het water erbij, breng aan de kook en verkruimel de bouillonblokjes boven de pan. Laat alles 10 min zachtjes doorkoken.\n'
-               '4. Pureer de soep met een staafmixer tot een gladde groene massa.\n'
-               '5. Roer de room erdoor.\n'
+           '1. Snijd de ui fijn en bak in een pan met boter glazig.'
+               '2. Snijd de aardappel in blokjes. Voeg de aardappel en ontdooide spinazie toe aan de ui.'
+               '3. Giet het water erbij, breng aan de kook en verkruimel de bouillonblokjes boven de pan. Laat alles 10 min zachtjes doorkoken.'
+               '4. Pureer de soep met een staafmixer tot een gladde groene massa.'
+               '5. Roer de room erdoor.'
                '6. Serveer de soep in borden of kommen en garneer eventueel met wat extra room, pijnboompitten en waterkers.'
        );
 
@@ -81,15 +81,15 @@ VALUES (
            25,
            'snacks','spinaziebladerdeeg.jpg',
            '1. Verwarm de oven voor op 200° graden.\n'
-               '2. Bak de spinazie in een grote pan tot deze geslonken is. Je kunt ook eventueel spinazie uit de diepvries ontdooien.\n'
-               '3. Laat de spinazie goed uitlekken en druk met de bolle kant van een lepel zo veel mogelijk vocht eruit.\n'
-               '4. Breng de spinazie op smaak met een flinke snuf peper.\n'
+               '2. Bak de spinazie in een grote pan tot deze geslonken is. Je kunt ook eventueel spinazie uit de diepvries ontdooien.'
+               '3. Laat de spinazie goed uitlekken en druk met de bolle kant van een lepel zo veel mogelijk vocht eruit.'
+               '4. Breng de spinazie op smaak met een flinke snuf peper.'
                '5. Rol het bladerdeeg uit.\n'
-               '6. Verdeel de spinazie over het bladerdeeg en kruimel hier de feta in kleine stukjes over.\n'
-               '7. Bestrooi met wat pijnboompitten.\n'
-               '8. Rol het bladerdeeg voorzichtig op.\n'
-               '9. Snijd met een scherp mes de rol in plakjes. Is dit lastig? Dan kun je de rol een half uurtje in de koelkast leggen zodat hij wat opstijft.\n'
-               '10. Leg de plakjes op een bakplaat met bakpapier en bestrijk met een los geklopt ei.\n'
+               '6. Verdeel de spinazie over het bladerdeeg en kruimel hier de feta in kleine stukjes over.'
+               '7. Bestrooi met wat pijnboompitten.'
+               '8. Rol het bladerdeeg voorzichtig op.'
+               '9. Snijd met een scherp mes de rol in plakjes. Is dit lastig? Dan kun je de rol een half uurtje in de koelkast leggen zodat hij wat opstijft.'
+               '10. Leg de plakjes op een bakplaat met bakpapier en bestrijk met een los geklopt ei.'
                '11. Bak de rolletjes 15 tot 20 minuten in de oven totdat ze goudbruin zijn.'
        );
 
@@ -109,12 +109,12 @@ VALUES (
            10,
            'middagmaal',
            'glutenvrije_pizza.jpg',
-           '1. Verwarm de oven voor op 225° graden.\n'
-               '2. Meng het meel, olijfolie, zout, bakpoeder en water. Kneed dit tot dat er een mooie bal ontstaat. Als het te droog is kun je een klein beetje extra water toevoegen.\n'
-               '3. Verdeel wat glutenvrij meel over het aanrecht of gebruik bakpapier en rol het deeg uit tot een platte cirkel.\n'
-               '4. Verdeel de tomatensaus met een lepel over de pizza.\n'
-               '5. Snijd de paprika en zongedroogde tomaten en leg deze vervolgens op de tomatensaus.\n'
-               '6. Strooi de Italiaanse kruiden, geraspte kaas en een snuf peper en zout hier overheen.\n'
+           '1. Verwarm de oven voor op 225° graden.'
+               '2. Meng het meel, olijfolie, zout, bakpoeder en water. Kneed dit tot dat er een mooie bal ontstaat. Als het te droog is kun je een klein beetje extra water toevoegen.'
+               '3. Verdeel wat glutenvrij meel over het aanrecht of gebruik bakpapier en rol het deeg uit tot een platte cirkel.'
+               '4. Verdeel de tomatensaus met een lepel over de pizza.'
+               '5. Snijd de paprika en zongedroogde tomaten en leg deze vervolgens op de tomatensaus.'
+               '6. Strooi de Italiaanse kruiden, geraspte kaas en een snuf peper en zout hier overheen.'
                '7. Bak de pizza voor 15 minuten op 225° graden.'
        );
 
@@ -140,10 +140,10 @@ VALUES (
            20,
            'dessert',
            'cranberry_compote.jpg',
-           '1. Verwarm het sinaasappelsap met de basterdsuiker in een pannetje.\n'
-               '2. Voeg de balsamico azijn en een snuf kaneel toe.\n'
-               '3. Voeg dan de cranberries toe en breng zachtjes aan de kook.\n'
-               '4. Roer af en toe door en wacht tot de cranberries zacht worden en uit elkaar vallen, waardoor er een saus ontstaat. Dit duurt ongeveer 15 minuten.\n\n'
+           '1. Verwarm het sinaasappelsap met de basterdsuiker in een pannetje.'
+               '2. Voeg de balsamico azijn en een snuf kaneel toe.'
+               '3. Voeg dan de cranberries toe en breng zachtjes aan de kook.'
+               '4. Roer af en toe door en wacht tot de cranberries zacht worden en uit elkaar vallen, waardoor er een saus ontstaat. Dit duurt ongeveer 15 minuten.'
                'De cranberry compote is lekker bij een stukje wild of bij het dessert en is een onmisbaar gerecht bij het kerstdiner. Bewaar de saus afgedekt in de koelkast. De cranberry compote is enkele weken tot maanden houdbaar.'
        );
 
@@ -168,12 +168,12 @@ VALUES (
            10,
            'snack',
            'snicker_balls.jpg',
-           '1. Als de dadels hard zijn, laat je ze even weken in een schaal met water. Als ze goed zacht zijn, is dit niet nodig.\n'
-               '2. Doe de pinda’s in een keukenmachine en maal ze tot kleinere stukjes. Haal ze er daarna weer uit.\n'
-               '3. Doe vervolgens de dadels in de keukenmachine en maal ze fijn.\n'
-               '4. Voeg de gemalen pinda’s, pindakaas, maple syrup, cacao poeder en snuf zout toe aan de dadels in de keukenmachine.\n'
-               '5. Meng alles goed totdat het een samenhangend mengsel vormt.\n'
-               '6. Maak met je handen balletjes van het mengsel.\n'
+           '1. Als de dadels hard zijn, laat je ze even weken in een schaal met water. Als ze goed zacht zijn, is dit niet nodig.'
+               '2. Doe de pinda’s in een keukenmachine en maal ze tot kleinere stukjes. Haal ze er daarna weer uit.'
+               '3. Doe vervolgens de dadels in de keukenmachine en maal ze fijn.'
+               '4. Voeg de gemalen pinda’s, pindakaas, maple syrup, cacao poeder en snuf zout toe aan de dadels in de keukenmachine.'
+               '5. Meng alles goed totdat het een samenhangend mengsel vormt.'
+               '6. Maak met je handen balletjes van het mengsel.'
                '7. Bewaar de balletjes het beste in de koelkast.'
        );
 INSERT INTO INGREDIENT(NAME, QUANTITY, UNIT, DISH_ID)
@@ -192,10 +192,10 @@ VALUES (
            50,
            'avondmaal',
            'babi_ketjap_spiezen.jpg',
-           '1. Snijd de varkenslappen in stukken en bestrooi met gemalen korianderzaad en gemalen gember. Voeg een scheutje ketjap toe en schep door elkaar. Laat minimaal een half uur marineren.\n' ||
-           '2. Snipper ondertussen het sjalotje en de knoflook zo fijn mogelijk.\n' ||
-           '3. Verhit in een pannetje een beetje olie en bak de ui en knoflook aan. Voeg de ketchup en de sambal toe en bak 1 minuutje mee. Voeg een scheut ketjap toe (ongeveer 100 ml) en breng aan de kook. Voeg ongeveer eenzelfde hoeveelheid water toe en laat het pruttelen tot het begint in te dikken.\n' ||
-           '4. Rijg het gemarineerde vlees aan de spiezen en bak in een pan (of op de BBQ) in ongeveer 7 minuten gaar.\n' ||
+           '1. Snijd de varkenslappen in stukken en bestrooi met gemalen korianderzaad en gemalen gember. Voeg een scheutje ketjap toe en schep door elkaar. Laat minimaal een half uur marineren.'
+           '2. Snipper ondertussen het sjalotje en de knoflook zo fijn mogelijk.'
+           '3. Verhit in een pannetje een beetje olie en bak de ui en knoflook aan. Voeg de ketchup en de sambal toe en bak 1 minuutje mee. Voeg een scheut ketjap toe (ongeveer 100 ml) en breng aan de kook. Voeg ongeveer eenzelfde hoeveelheid water toe en laat het pruttelen tot het begint in te dikken.'
+           '4. Rijg het gemarineerde vlees aan de spiezen en bak in een pan (of op de BBQ) in ongeveer 7 minuten gaar.' 
            '5. Serveer de spiezen met de ketjap saus erover en garneer met gebakken uitjes en bosui. Lekker met rijst en/of roerbakgroente.'
        );
 
