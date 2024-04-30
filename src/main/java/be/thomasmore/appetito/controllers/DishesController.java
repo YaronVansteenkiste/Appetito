05959195f981c02ead91def42e8b8bacc1cb3795
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-04-30 11:19:46 +0200

    dishes filter controller created

--- src/main/java/be/thomasmore/appetito/controllers/DishesController.java
+++ src/main/java/be/thomasmore/appetito/controllers/DishesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DishesController {
@@ -26,8 +27,16 @@ public class DishesController {
 
 
     @GetMapping("/dishes/filter")
-    public String dishesFilter(Model model) {
+    public String dishesFilter(Model model, @RequestParam(required = false) String dietPreferences,
+                               @RequestParam(required = false) Integer preparationTime,
+                               @RequestParam(required = false) String preparation,
+                               @RequestParam(required = false) String occasion) {
         Iterable<Dish>allDishes= dishesRepository.findAll();
+        allDishes = dishesRepository.findFilteredDishes(dietPreferences,preparationTime,preparation,occasion);
+        model.addAttribute("dietPreferences",dietPreferences);
+        model.addAttribute("preparationTime",preparationTime);
+        model.addAttribute("preparation",preparation);
+        model.addAttribute("occasion",occasion);
         model.addAttribute("count",allDishes.spliterator().estimateSize());
         model.addAttribute("alldishes",allDishes);
         model.addAttribute("allIngredients",ingredientsRepository.findAll());
