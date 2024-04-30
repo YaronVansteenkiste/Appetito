dd514f6f5bb2f86b368fe4f2fb33449224b11d02
yaronvsk <yaronvsk@googlemail.com>
2024-04-30 13:23:01 +0200

    30/04: Filteren op maaltijd

--- src/main/java/be/thomasmore/appetito/controllers/DishDetailController.java
+++ src/main/java/be/thomasmore/appetito/controllers/DishDetailController.java
@@ -1,4 +1,5 @@
 package be.thomasmore.appetito.controllers;
+
 import be.thomasmore.appetito.model.Dish;
 import be.thomasmore.appetito.repositories.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,24 +8,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Controller
 public class DishDetailController {
 
     @Autowired
-    private DishRepository dishesRepository;
+    private DishRepository dishRepository;
 
     @GetMapping({"/dishdetails/{id}" , "/dishdetails"})
     public String dishDetail(Model model, @PathVariable(required = false) Integer id){
-        final Iterable<Dish> allDishes = dishesRepository.findAll();
+        final Iterable<Dish> allDishes = dishRepository.findAll();
         model.addAttribute("dishes", allDishes);
 
         if(id == null){
             return "error";
         }
 
-        Optional<Dish> dishFromDB = dishesRepository.findById(id);
+        Optional<Dish> dishFromDB = dishRepository.findById(id);
         if(dishFromDB.isPresent())
         {
             model.addAttribute("dish", dishFromDB.get());
--- src/main/java/be/thomasmore/appetito/repositories/DishRepository.java
+++ src/main/java/be/thomasmore/appetito/repositories/DishRepository.java
@@ -3,12 +3,13 @@ package be.thomasmore.appetito.repositories;
 import be.thomasmore.appetito.model.Dish;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface DishRepository extends CrudRepository<Dish, Integer> {
-    @Query("select d from Dish d where d.dietPreferences " +
-            "like %?1% and d.preparationTime <= ?2 and d.preparation " +
-            "like %?3% and d.occasion like %?4%")
-    Iterable<Dish> findFilteredDishes(String dietPreferences, Integer preparationTime, String preparation, String occasion);
+    @Query("select d from Dish d where (:dietPreferences is null or d.dietPreferences like %:dietPreferences%) " +
+            "and (:preparationTime is null or d.preparationTime <= :preparationTime) and (:preparation is null or d.preparation " +
+            "like %:preparation%) and (:occasion is null or d.occasion like %:occasion%)")
+    Iterable<Dish> findFilteredDishes(@Param("dietPreferences") String dietPreferences, @Param("preparationTime") Integer preparationTime, @Param("preparation") String preparation, @Param("occasion") String occasion);
 
 
     @Query("select d from Dish d where d.name like %?1%")
