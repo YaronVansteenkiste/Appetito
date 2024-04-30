48f86048397a9157b2fa90cd5001a720833b8ab0
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-04-30 12:35:49 +0200

    30/04 - Dishdetail verwerkt (Na denk hoe de ingredient er in gaat)

--- src/main/java/be/thomasmore/appetito/controllers/DishDetailController.java
+++ src/main/java/be/thomasmore/appetito/controllers/DishDetailController.java
@@ -1,21 +1,19 @@
 package be.thomasmore.appetito.controllers;
-
 import be.thomasmore.appetito.model.Dish;
-import be.thomasmore.appetito.repositories.DishesRepository;
+import be.thomasmore.appetito.repositories.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Controller
 public class DishDetailController {
 
     @Autowired
-    private DishesRepository dishesRepository;
+    private DishRepository dishesRepository;
 
     @GetMapping({"/dishdetails/{id}" , "/dishdetails"})
     public String dishDetail(Model model, @PathVariable(required = false) Integer id){
