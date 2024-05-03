e9b8c4eec3a83ba2cd9f0991089de7ccda0e2f15
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-05-03 09:11:54 +0200

    03/05 - X-dagen Menu (html form gemaakt, eventjes postmapping weg)

--- src/main/java/be/thomasmore/appetito/controllers/XDagenMenuController.java
+++ src/main/java/be/thomasmore/appetito/controllers/XDagenMenuController.java
@@ -1,13 +1,13 @@
 package be.thomasmore.appetito.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class XDagenMenuController {
     @GetMapping("/x-dagenmenu")
-        public String xdagenmenu(Model model){
+    public String xdagenmenu() {
         return "menu";
     }
 }
