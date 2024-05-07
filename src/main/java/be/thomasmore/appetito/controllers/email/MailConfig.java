6afd080eb9952ca8093493866b140a85c48ddd8a
ahmedk1991 <ahmed.kerkri@outlook.com>
2024-05-07 13:28:23 +0200

    mailsysteem toegevoegd nog niet helemaal af !!

---
+++ src/main/java/be/thomasmore/appetito/controllers/email/MailConfig.java
@@ -0,0 +1,36 @@
+package be.thomasmore.appetito.controllers.email;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        mailSender.getJavaMailProperties().put("mail.smtp.starttls.enable", "true");
+        return mailSender;
+    }
+}
