b4c358e4031e46cf4fc207ba25c98fbf29e9c614
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2024-05-08 11:21:49 +0200

    08/05: Lijst met gebruikers + admin dashboard

--- src/main/java/be/thomasmore/appetito/controllers/admin/AdminController.java
+++ src/main/java/be/thomasmore/appetito/controllers/admin/AdminController.java
@@ -1,6 +1,10 @@
 package be.thomasmore.appetito.controllers.admin;
 
+import be.thomasmore.appetito.model.Chef;
+import be.thomasmore.appetito.repositories.ChefRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AdminController {
 
+    @Autowired
+    private ChefRepository chefRepository;
+
 
     @GetMapping("/")
     public String Dashboard() {
@@ -16,7 +23,9 @@ public class AdminController {
     }
 
     @GetMapping("/users")
-    public String users() {
+    public String users(Model model) {
+        Iterable<Chef> chefs = chefRepository.findAll();
+        model.addAttribute("chefs", chefs);
         return "admin/users";
     }
 }
