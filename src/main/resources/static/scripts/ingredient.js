d30e58abbd5d90a6d79f2b3aa6c2f21fb540d35e
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2024-05-09 15:29:05 +0200

    09/05: Recept aanpassen aan personen

--- src/main/resources/static/scripts/ingredient.js
+++ src/main/resources/static/scripts/ingredient.js
@@ -1,9 +1,36 @@
-function addPersons() {
-    const personsEl = document.getElementById('persons');
-    personsEl.innerText = parseInt(personsEl.textContent)+1;
-}
-
 function subtractPersons() {
     const personsEl = document.getElementById('persons');
-    personsEl.innerText = parseInt(personsEl.textContent) +1;
+    let amountPersons = parseInt(personsEl.textContent);
+
+    if (amountPersons > 1) {
+        amountPersons -= 1;
+        const quantities = document.querySelectorAll('.quantity')
+        quantities.forEach(quantity => {
+            let quantityData = quantity.getAttribute('data-quantity');
+            if (quantityData == 0) {
+                quantity.innerText = '';
+            } else {
+                quantity.innerText = (quantityData * (amountPersons)).toFixed(1);
+            }
+        })
+        personsEl.innerText = amountPersons;
+    }
+}
+
+function addPersons() {
+    const personsEl = document.getElementById('persons');
+    let amountPersons = parseInt(personsEl.textContent);
+
+    amountPersons += 1;
+    const quantities = document.querySelectorAll('.quantity')
+    quantities.forEach(quantity => {
+        let quantityData = quantity.getAttribute('data-quantity');
+
+        if (quantityData == 0) {
+            quantity.innerText = '';
+        } else {
+            quantity.innerText = (quantityData * (amountPersons)).toFixed(1);
+        }
+    })
+    personsEl.innerText = amountPersons;
 }
