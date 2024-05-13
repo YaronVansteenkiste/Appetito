1f7e0f864e03b1f041085476487033610df4f7c0
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2024-05-13 16:02:40 +0200

    13/05: add groceries to list

--- src/main/java/be/thomasmore/appetito/controllers/GroceriesController.java
+++ src/main/java/be/thomasmore/appetito/controllers/GroceriesController.java
@@ -1,7 +1,9 @@
 package be.thomasmore.appetito.controllers;
 
+import be.thomasmore.appetito.model.Chef;
 import be.thomasmore.appetito.model.Grocery;
 import be.thomasmore.appetito.model.Ingredient;
+import be.thomasmore.appetito.repositories.ChefRepository;
 import be.thomasmore.appetito.repositories.ErrandsRepository;
 import be.thomasmore.appetito.repositories.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Controller
 public class GroceriesController {
@@ -20,10 +24,21 @@ public class GroceriesController {
 
     @Autowired
     IngredientRepository ingredientRepository;
+    @Autowired
+    ChefRepository chefRepository;
 
-    @GetMapping("/groceries/{id}")
-    public String groceries(Model model, @PathVariable(required = false) Integer id) {
-        Optional<Grocery> groceryFromDB = errandsRepository.findById(id);
+    private Logger logger = Logger.getLogger(GroceriesController.class.getName());
+
+    @GetMapping("/groceries/")
+    public String groceries(Model model, Principal principal) {
+        if (principal == null) {
+            return "redirect:/user/login";
+        }
+        Chef chef = chefRepository.findByUsername(principal.getName());
+        logger.info("Chef: " + chef.getName());
+        Integer chefId = chef.getId();
+
+        Optional<Grocery> groceryFromDB = errandsRepository.findById(chefId);
         groceryFromDB.ifPresent(grocery -> {
             model.addAttribute("grocery", grocery);
         });
