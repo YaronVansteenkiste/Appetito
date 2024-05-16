6bd50fbcb7a597aa89d8ea327cf7db45883e030c
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-05-16 12:31:30 +0200

    button veranderd naar hartje

--- src/main/java/be/thomasmore/appetito/repositories/BeverageRepository.java
+++ src/main/java/be/thomasmore/appetito/repositories/BeverageRepository.java
@@ -4,4 +4,5 @@ import be.thomasmore.appetito.model.Beverage;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BeverageRepository extends CrudRepository<Beverage, Integer> {
+
 }
