d150b0db6803466242ea96a194a7cfb2ed980568
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-05-17 11:51:54 +0200

    17/05 - deel functies gecopieert van master naar AdminController

--- src/main/java/be/thomasmore/appetito/controllers/admin/AdminController.java
+++ src/main/java/be/thomasmore/appetito/controllers/admin/AdminController.java
@@ -1,13 +1,17 @@
 package be.thomasmore.appetito.controllers.admin;
 
+import be.thomasmore.appetito.model.Beverage;
 import be.thomasmore.appetito.model.Chef;
+import be.thomasmore.appetito.model.Dish;
+import be.thomasmore.appetito.repositories.BeverageRepository;
 import be.thomasmore.appetito.repositories.ChefRepository;
+import be.thomasmore.appetito.repositories.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequestMapping("/admin")
 @Controller
@@ -16,9 +20,18 @@ public class AdminController {
     @Autowired
     private ChefRepository chefRepository;
 
+    @Autowired
+    private DishRepository dishRepository;
+
+    @Autowired
+    private BeverageRepository beverageRepository;
+
+
 
     @GetMapping("/")
-    public String Dashboard() {
+    public String Dashboard(Model model) {
+        Iterable<Dish> dishes = dishRepository.findAll();
+        model.addAttribute("dishes", dishes);
         return "admin/dashboard";
     }
 
@@ -27,5 +40,21 @@ public class AdminController {
         Iterable<Chef> chefs = chefRepository.findAll();
         model.addAttribute("chefs", chefs);
         return "admin/users";
+    }
+
+    @PostMapping("/toggle/dish/{id}")
+    public String updateDishToggleState(@PathVariable("id") int id, @RequestParam boolean active) {
+        Dish dish = dishRepository.findById(id).orElseThrow(() -> new IllegalStateException("Dish not found"));
+        dish.setActive(active);
+        dishRepository.save(dish);
+        return "redirect:/";
+    }
+
+    @PostMapping("/toggle/beverage/{id}")
+    public String updateBeverageToggleState(@PathVariable("id") int id, @RequestParam boolean active){
+        Beverage beverage = beverageRepository.findById(id).orElseThrow(() -> new IllegalStateException("Beverage not found"));
+        beverage.setActive(active);
+        beverageRepository.save(beverage);
+        return "redirect:/dishdetails/" + id;
     }
 }
