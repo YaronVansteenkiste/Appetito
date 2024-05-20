8213b553a7c4f9ec2e1a88a6a12e4610d76e8d76
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-05-20 12:59:47 +0200

    20/05 - Nieuwe Cards kunnen toevoegen voor Dranken. Probleem: Slaat de nieuwe kaarten niet op.

--- src/main/resources/static/scripts/addbev.js
+++ src/main/resources/static/scripts/addbev.js
@@ -16,3 +16,32 @@ function addBev() {
     textContainer.appendChild(textInput);
     imageContainer.appendChild(imageInput);
 }
+
+
+document.querySelector('input[type="button"]').addEventListener('click', addDrink);
+function addDrink() {
+    let cardContainer = document.getElementById("cardContainer").getElementsByClassName("row justify-content-center")[0];
+
+    let card = document.createElement("div");
+    card.className = "col-lg-4 text-center mt-3";
+
+    card.innerHTML = `
+        <article class="card">
+            <div style="position: relative; width: 100%; height: 200px; overflow: hidden;">
+                <img src="" class="position-absolute top-0 start-0 w-100 h-100"
+                     style="object-fit: cover;" alt="Image Drink">
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="beverageName" class="form-label">Beverage Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter beverage name">
+                </div>
+                <div class="mb-3">
+                    <label for="beverageImage" class="form-label">Beverage Image</label>
+                    <input type="file" class="form-control" name="imageFiles" accept="image/*">
+                </div>
+            </div>
+        </article>
+    `;
+    cardContainer.appendChild(card);
+}
