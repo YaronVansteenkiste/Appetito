6282267c006f1cecc19dcce828a799449d7faee8
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-05-20 17:36:43 +0200

    20/05 - Card terug op active zetten. Probleem: '+' knop voegt 2 kaarten toe ipv 1.

--- src/main/resources/static/scripts/active_inactive.js
+++ src/main/resources/static/scripts/active_inactive.js
@@ -2,6 +2,17 @@ function toggleInactive(button) {
     let card = button.closest('.card');
     let isActive = !card.classList.contains('inactive');
     if (isActive) {
-        card.innerHTML = '<div class="card-body"><p class="text-muted">Het drank is onbeschikbaar</p></div>';
+        let originalContent = card.innerHTML;
+
+        card.innerHTML = '<div class="card-body"><p class="text-muted">This drink is inactive</p><button type="button" class="btn btn-success" onclick="toggleInactive(this)">Reactivate</button></div>';
+        card.classList.add('inactive');
+
+        card.dataset.originalContent = originalContent;
+    } else {
+
+        card.innerHTML = card.dataset.originalContent;
+        card.classList.remove('inactive');
+
+        delete card.dataset.originalContent;
     }
 }
