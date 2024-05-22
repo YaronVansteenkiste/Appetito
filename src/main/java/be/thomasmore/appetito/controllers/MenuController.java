6d6cfdee955739036428292b346f2d7e5b499c63
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2024-05-22 10:38:27 +0200

    22/05: troubleshooting

--- src/main/java/be/thomasmore/appetito/controllers/MenuController.java
+++ src/main/java/be/thomasmore/appetito/controllers/MenuController.java
@@ -10,11 +10,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -22,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class MenuController {
     @Autowired
