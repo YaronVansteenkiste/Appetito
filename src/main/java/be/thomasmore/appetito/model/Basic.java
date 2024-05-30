a9c8743f38250f1dc3c164bc066f7ea238f9d175
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-05-30 15:31:26 +0200

    entity class Basic

---
+++ src/main/java/be/thomasmore/appetito/model/Basic.java
@@ -0,0 +1,56 @@
+package be.thomasmore.appetito.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jdk.jfr.Enabled;
+
+@Entity
+public class Basic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String action;
+
+    private String description;
+
+    private String image;
+
+    public Basic() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
