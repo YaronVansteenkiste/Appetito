846e1c9ba0043d49ee19b272c2fc0ae599b0a467
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2024-06-03 20:37:22 +0200

    03/06: Wizard progress bar

--- src/main/resources/static/scripts/fixformat.js
+++ src/main/resources/static/scripts/fixformat.js
@@ -1,7 +1,6 @@
 document.getElementById('dishForm').addEventListener('submit', function(e) {
     const prepTimeInput = document.getElementById('prepTime');
     const prepTime = prepTimeInput.value;
-    alert(prepTime.split(':').length);
     if (prepTime.split(':').length === 2) {
         prepTimeInput.value = prepTime + ':00';
     }
